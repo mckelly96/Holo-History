a0242899c210346ba461fc07eb374adf3adbcd1d
nrewkowski <nrewkowski11@gmail.com>
2018-11-28 15:27:45 -0500

    fixed wrong comment

--- Assets/GameStateController.cs
+++ Assets/GameStateController.cs
@@ -10,7 +10,9 @@ public class GameStateController : MonoBehaviour {
 	//because they aren't really part of the game right now...they are mostly workarounds
 
 	possibleGameStatesRelatedToAnna currentGameState=possibleGameStatesRelatedToAnna.Introduce_Anna;
-	public List<AudioClip> audioClipsCorrespondingToEachOfAnnasStates; //this array should have the same length as possibleGameStatesRelatedToAnna. some other more general solution would need
+	public List<AudioClip> audioClipsCorrespondingToEachOfAnnasStates; //this array should have the same length as possibleGameStatesRelatedToAnna. 
+	//set these clips somewhere (maybe in C#? or don't use an array and just have a couple of variable as public AudioClip clip1,clip2,...., which is bad style but would work)
+	//some other more general solution would need
 	//to be implemented for more than 1 actor (eg. each actor should have an array like this and the controller will tell them all to activate. timing would be an issue)
 
 	//consider adding an array of audio sources with length=possibleGameStatesRelatedToAnna.length so that the narrator and anna can swap talking. 
@@ -27,7 +29,7 @@ public class GameStateController : MonoBehaviour {
 	//this exists to guarantee that they leave the room.
 
 	bool waitingForUserToEnterRoom=true; //this will be set to true whenever Anna finishes her speech and then the user leaves the room. 
-	//this should never be the same as waitingForUserToLeaveRoom
+	//this should never be true at the same time as waitingForUserToLeaveRoom (they are both false while anna is talking)
 	//if the user enters the room without looking at the marker, then NOTHING should happen. they MUST look at the marker to start the next part
 	
 	// Use this for initialization
@@ -48,7 +50,7 @@ public class GameStateController : MonoBehaviour {
 			//assume markers are positioned and handle game states.
 			//************STATE FLOW************** */  START>>>>>>wait for user to enter room>anna state 0>wait for exit room>wait for enter room>anna state 1>....
 			//only check for markers AFTER she's done talking (check in the audioclip (isPlaying) for the character)
-			
+
 			//marker "tracking" will need to be simulated since we can't use vuforia AND do screen mirroring on the hololens.
 			//we can use 3D angle calculations to determine if the user is more or less looking at the virtual marker; (cos(THETA)=(a dot b)/(|a|*|b|), with a = forward vector of camera
 			//and b as the vector from camera location to a marker.
